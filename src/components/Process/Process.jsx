b66ba356236473dfbb080877bdbc6c7e139a8af3
Abhishek <abhishekuhindiholi@gmail.com>
2026-07-21 22:45:11 +0530

    Made  Our Process responsive

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -12,8 +12,8 @@ import { BsTruck } from "react-icons/bs";
 const Process = () => {
   const renderSteps = steps.map((item) => {
     return (
-      <div className={`flex-1 ${item.id % 2 == 0 ? '-mt-100' : ''}`}>
-        <span className="flex rounded-full justify-center items-center text-white w-18 h-18 text-8xl bg-zinc-800 outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed">
+      <div className={`flex-1 basis-[300px] ${item.id % 2 == 0 ? 'md:-mt-100' : ''}`}>
+        <span className="flex rounded-full justify-center items-center text-white md:mx-auto w-18 h-18 text-8xl bg-zinc-800 outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed">
           {item.number}
         </span>
         <div className="flex items-center mt-10 gap-x-5">
@@ -34,7 +34,7 @@ const Process = () => {
         <div className="w-fit mr-auto">
           <Heading highlight="Our" heading="Process" />
         </div>
-        <div className="flex flex-wrap items-center justify-center mt-20 pt-50">{renderSteps}</div>
+        <div className="flex flex-wrap items-center gap-y-15 justify-center md:mt-20 mt-10 md:pt-50">{renderSteps}</div>
       </div>
     </section>
   );
